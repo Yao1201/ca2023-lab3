841cb97abbd3dc29ee131db748e6a21486a32ae9
Yao1201 <147422451+Yao1201@users.noreply.github.com>
2023-11-27 15:51:14 +0800

    Update InstructionDecode.scala

--- src/main/scala/riscv/core/InstructionDecode.scala
+++ src/main/scala/riscv/core/InstructionDecode.scala
@@ -195,7 +195,8 @@ class InstructionDecode extends Module {
   )
 
   // lab3(InstructionDecode) begin
-
+  io.memory_read_enable := (opcode === InstructionTypes.L) || (opcode === Instructions.lui) 
+  io.memory_write_enable := (opcode === InstructionTypes.S)
   // lab3(InstructionDecode) end
 
   io.wb_reg_write_source := MuxCase(
