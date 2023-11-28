406aa26372948d23a8f54638db62d0c7f0cfa1e1
Yao1201 <147422451+Yao1201@users.noreply.github.com>
2023-11-28 15:51:15 +0800

    Update CPUTest.scala

--- src/test/scala/riscv/singlecycle/CPUTest.scala
+++ src/test/scala/riscv/singlecycle/CPUTest.scala
@@ -63,6 +63,29 @@ class TestTopModule(exeFilename: String) extends Module {
   io.mem_debug_read_data    := mem.io.debug_read_data
 }
 
+// add HammingdistTest to test the result
+class HammingdistTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior.of("Single Cycle CPU")
+  it should "Calculate hammingdistance of two sequences" in {
+    test(new TestTopModule("Hammingdist.asmbin")).withAnnotations(TestAnnotations.annos) { c =>
+      for (i <- 1 to 50) {
+        c.clock.step(1000)
+        c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+      }
+      
+      c.io.mem_debug_read_address.poke(4.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(21.U)
+      c.io.mem_debug_read_address.poke(8.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(63.U)
+      c.io.mem_debug_read_address.poke(12.U)
+      c.clock.step()
+      c.io.mem_debug_read_data.expect(0.U)
+    }
+  }
+}
+
 class FibonacciTest extends AnyFlatSpec with ChiselScalatestTester {
   behavior.of("Single Cycle CPU")
   it should "recursively calculate Fibonacci(10)" in {
